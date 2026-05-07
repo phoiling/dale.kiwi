2bb5dfba3219c0a0f8037add8fcf9fa00f2a8694
Dale leyland <daleleyland@icloud.com>
2026-05-08 09:49:50 +1200

    tired to fix vercell anylitis again

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.addEventListener
+import { inject } from '@vercel/analytics';
+
+inject();
+
 const toggle = document.getElementById('day-night-toggle');
 toggle.addEventListener('click',() => {
     toggle.src = toggle.src.includes('sun') ? 'Images/moon_icon.png' : 'Images/sun_icon.png';
 });
-
-import { inject } from '@vercel/analytics';
-
-inject();
