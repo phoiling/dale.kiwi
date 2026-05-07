5e6b0b78cf794ab8bf1f2722cf6d53a889b52ba1
Dale leyland <daleleyland@icloud.com>
2026-05-08 09:38:45 +1200

    added vercell anyliticks

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+document.addEventListener
 const toggle = document.getElementById('day-night-toggle');
 toggle.addEventListener('click',() => {
-    toggle.src = toggle.src.includes('sun') ? 'Images/moon.png' : 'Images/sun.png';
+    toggle.src = toggle.src.includes('sun') ? 'Images/moon_icon.png' : 'Images/sun_icon.png';
 });
+
+import { inject } from '@vercel/analytics';
+
+inject();
