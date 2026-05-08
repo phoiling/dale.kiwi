0ca59517e036e1c745da69f0b2758ce8ef29b87f
Dale leyland <daleleyland@icloud.com>
2026-05-08 14:18:54 +1200

    atempt 3 of fixing vercell anyliticks

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import { inject } from '@vercel/analytics';
-
-inject();
-
 const toggle = document.getElementById('day-night-toggle');
 toggle.addEventListener('click',() => {
     toggle.src = toggle.src.includes('sun') ? 'Images/moon_icon.png' : 'Images/sun_icon.png';
