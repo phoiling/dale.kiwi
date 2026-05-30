fdaaf788bd77ffab331658aa15ede15a3ff1be87
Dale leyland <daleleyland@icloud.com>
2026-05-31 09:52:53 +1200

    dleeted root js

--- script.js
+++
@@ -1,4 +0,0 @@
-const toggle = document.getElementById('day-night-toggle');
-toggle.addEventListener('click',() => {
-    toggle.src = toggle.src.includes('sun') ? 'Images/moon_icon.png' : 'Images/sun_icon.png';
-});
