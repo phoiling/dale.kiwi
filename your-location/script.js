c13b3f8de91a0467c277a6257352836a99d9bee6
Dale leyland <daleleyland@icloud.com>
2026-05-31 10:22:52 +1200

    added circle

--- your-location/script.js
+++ your-location/script.js
@@ -1,5 +1,13 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+var circle = L.circle([51.505, -0.09],{
+    color: 'blue',
+    fillColor: '#2799d2',
+    fillOpacty:0.5,
+    raidus:500
+
+}).addTo(map);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
