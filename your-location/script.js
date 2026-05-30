876a14f43f271a5fd33fea7721cbbfd21a5c53d9
Dale leyland <daleleyland@icloud.com>
2026-05-31 09:45:21 +1200

    hopefly fixed leaflet axcessblocked

--- your-location/script.js
+++ your-location/script.js
@@ -2,5 +2,7 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    referrerPolicy: 'no-referrer-when-downgrade',
+    headers: {'User-Agent': 'websire_that_finds_your_location_with_ip/1.0'}
 }).addTo(map);
