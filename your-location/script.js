269094e4adf53190f64405f8bec97e0774d1a129
Dale leyland <daleleyland@icloud.com>
2026-05-31 12:15:43 +1200

    added map centering

--- your-location/script.js
+++ your-location/script.js
@@ -1,13 +1,12 @@
 fetch("https://geolocation-db.com/json/")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-    }
-)
+        const lat = data.latitude
+        const long = data.longitude
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([lat, long], 13);
 
-var circle = L.circle([51.505, -0.09],{
+var circle = L.circle([lat, long],{
     color: 'blue',
     fillColor: '#2799d2',
     fillOpacty:0.5,
@@ -21,3 +20,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     referrerPolicy: 'no-referrer-when-downgrade',
     headers: {'User-Agent': 'websire_that_finds_your_location_with_ip/1.0'}
 }).addTo(map);
+    })
